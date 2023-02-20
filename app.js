e7860d01006945d28b4163e21849f88a862be4e2
Leniyou <carlos.marte.2408@gmail.com>
2023-02-20 15:21:06 -0400

    Update app.js and add .babelrc

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const { version } = require('./package.json')
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 8080
 
-// app.get('/health', (req, res) => {
-//   res.send('ok')
-// });
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
-// app.get('/version', (req, res) => {
-//   res.send(version)
-// });
+app.get('/version', (req, res) => {
+  res.send(version)
+})
 
 app.use(express.static('dist'))
 
